b8396f226b0ef5e6ca90bdffaed9181633e9c8d6
Hamza Khan <hghauri30@gmail.com>
2025-05-11 02:17:52 +0500

    added axios call in stream

--- Controllers/download.js
+++ Controllers/download.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const insertSong = require('../services/insertToDb');
 const downloadMp3 = require('../services/downloadMp3');
 const uploadToStorage = require('../services/uploadToStorage');
-const https = require('https');
+
 
 
 
@@ -17,14 +17,17 @@ const fetchData = async (options) => {
 };
 // This function fetches the file from the given URL and passes the stream to a custom upload function
 const streamToStorage = async (url, uploadStreamFunc) => {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      // Instead of buffering, we stream the response to storage
-      uploadStreamFunc(res)
-        .then(resolve)
-        .catch(reject);
-    }).on('error', reject);
-  });
+  try {
+    const response = await axios.get(url, {
+      responseType: 'stream',
+      maxRedirects: 5, // follow redirects
+    });
+
+    return await uploadStreamFunc(response.data);
+  } catch (err) {
+    console.error('Axios stream error:', err.message);
+    throw new Error('Stream failed on Vercel');
+  }
 };
 
 const downloadSongController = async (req, res) => {
@@ -45,24 +48,25 @@ const downloadSongController = async (req, res) => {
       }
     };
 
-    const response = await fetchData(downloadOptions);
-    console.log("Response from download", response);
-    console.log("Debuging");
+    // const response = await fetchData(downloadOptions);
+    // console.log("Response from download", response);
+    // console.log("Debuging");
 
-    // const response={
-    //   link: 'https://epsilon.123tokyo.xyz/get.php/0/49/OhLPsjScQCc.mp3?n=Lady%20Gaga%2C%20Bruno%20Mars%20-%20Die%20With%20A%20Smile%20%28Official%20Music%20Video%29&uT=R&uN=aGFtemFraGFuNjA%3D&h=vNUy_lpCksjf-W5ByfFdRw&s=1746915973&uT=R&uN=aGFtemFraGFuNjA%3D',
-    //   title: 'Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video)',
-    //   filesize: 3375628,
-    //   progress: 100,
-    //   duration: 204.06857221242,
-    //   status: 'ok',
-    //   msg: 'success'
-    // }
-
-
+    const response={
+      link: 'https://theta.123tokyo.xyz/get.php/4/72/XO8wew38VM8.mp3?n=MILLIONAIRE%20SONG%20%28Full%20Video%29_%20%40YoYoHoneySingh%20_%20GLORY%20_%20BHUSHAN%20KUMAR&uT=R&uN=aGFtemFraGFuNjA%3D&h=fBWS7RLJ0M1Xx-s7_z-wqQ&s=1746918572&uT=R&uN=aGFtemFraGFuNjA%3D',
+      title: 'MILLIONAIRE_SONG_(Full_Video):_@YoYoHoneySingh__|_GLORY_|_BHUSHAN_KUMAR.mp3',
+      filesize: 3375628,
+      progress: 100,
+      duration: 204.06857221242,
+      status: 'ok',
+      msg: 'success'
+    }
 
 
-    //const fileBuffer = await downloadMp3(response.link);
+
+
+    const fileBuffer = await downloadMp3(response.link);
+    console.log("File buffer", fileBuffer);
     const fileName = `${response.title.replace(/ /g, '_')}.mp3`;
     console.log("File name", fileName);
     //console.log("File buffer", fileBuffer);
